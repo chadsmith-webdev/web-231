055d239ab4eb8d8f22168fea722faba48eca2c93
Chad Smith <smithchadlamont@gmail.com>
2026-08-21 10:32:44 -0500

    Completed chapter work

---
+++ week-2/project02-02/project02-02.js
@@ -0,0 +1,10 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-02
+
+      Application to test for completed form
+      Author: Chad Smith 
+      Date: 2026-08-21   
+
+      Filename: project02-02.js
+ */
