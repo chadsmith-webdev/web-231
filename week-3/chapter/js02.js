c37947eb7f257ee56b36077bb99b7ef747ad1e8e
Chad Smith <smithchadlamont@gmail.com>
2026-08-24 11:16:26 -0500

    Created setupForm function and linked js file to html file.

---
+++ week-3/chapter/js02.js
@@ -0,0 +1,20 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Chapter case
+
+      Fan Trick Fine Art Photography
+      Variables and functions
+      Author: Chad Smith 
+      Date: 2026-08-24   
+
+      Filename: js02.js
+ */
+
+// set the form's default values
+function setupForm() {
+  document.getElementById("photoNum").value = 1;
+  document.getElementById("photoHrs").value = 2;
+  document.getElementById("makeBook").checked = false;
+  document.getElementById("photoRights").checked = false;
+  document.getElementById("photoDist").value = 0;
+}
