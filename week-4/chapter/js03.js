ca73644232fe8ead0d88835c0fae22806e76c836
Chad Smith <smithchadlamont@gmail.com>
2026-08-31 13:45:14 -0500

    Initial setup complete

---
+++ week-4/chapter/js03.js
@@ -0,0 +1,22 @@
+/*    JavaScript 7th Edition
+     Chapter 3
+     Chapter case
+
+     Tipton Turbines
+     Program to display games results in a web table
+     Author: Chad Smith
+     Date: 2026-08-31  
+
+     Filename: js03.js
+ */
+
+// Days of the week
+let weekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
