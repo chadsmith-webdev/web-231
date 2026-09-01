c77c8bfd13b760ef9a351681ceed3cfefdf6d57a
Chad Smith <smithchadlamont@gmail.com>
2026-09-01 10:38:43 -0500

    Add function to show game information into the calendar

--- week-4/chapter/js03.js
+++ week-4/chapter/js03.js
@@ -38,3 +38,29 @@ function addWeekDays() {
     i++;
   }
 }
+
+window.addEventListener("load", showGames);
+
+// function to write game information into the calendar
+function showGames() {
+  for (let i = 0; i < gameDates.length; i++) {
+    let gameInfo = "";
+
+    // open the paragraph
+    gameInfo += "<p>";
+
+    // include the opponent
+    gameInfo += gameOpponents[i] + "<br>";
+
+    // include the result and score
+    gameInfo +=
+      gameResults[i] + ": (" + runsScored[i] + " - " + runsAllowed[i] + ")";
+
+    // close the paragraph
+    gameInfo += "</p>";
+
+    // write the information into a table cell
+    let tableCell = document.getElementById(gameDates[i]);
+    tableCell.insertAdjacentHTML("beforeEnd", gameInfo);
+  }
+}
